7c9fc4617f3f98bf4953cc94cd4f8c78d804c7df
Kalaiselvan <ikalaiselvan@users.noreply.github.com>
2023-03-07 22:00:42 +0530

    first commit

--- index.js
+++ index.js
@@ -9,11 +9,10 @@ console.log(process.env.MONGO_URL)
 
 const app = express();
 
-const PORT = 4000;  
+const PORT = process.env.PORT; 
   
   // const MONGO_URL = "mongodb://127.0.0.1";           // no need to mention default port
-  // const MONGO_URL = process.env.MONG O_URL              // or defaultt port(27017)
-  const MONGO_URL = "mongodb+srv://kalaiselvan:kalai6565@cluster0.oiqqgzq.mongodb.net"
+  const MONGO_URL = process.env.MONGO_URL              // or defaultt port(27017)
 // const MONGO_URL = "mongodb://localhost:27017";       // or
 const client = new MongoClient(MONGO_URL);
 await client.connect();
